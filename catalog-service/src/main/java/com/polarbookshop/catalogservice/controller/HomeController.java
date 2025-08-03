82ed52c5b29abedce0e3908c90423b847d2e0370
Gaurav Kamble <gauravkamble909@gmail.com>
2025-08-03 18:36:59 +0530

    Chapter 2 - externalize configuration. config server via git

--- catalog-service/src/main/java/com/polarbookshop/catalogservice/controller/HomeController.java
+++ catalog-service/src/main/java/com/polarbookshop/catalogservice/controller/HomeController.java
@@ -1,13 +1,18 @@
 package com.polarbookshop.catalogservice.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+    @Value("${polar.greetings}")
+    private String greetings;
+
     @GetMapping("/")
-    public String getGreetings(){
-        return "Welcome to the book catalog!!";
+    public String getGreetings()
+    {
+        return greetings;
     }
 }
